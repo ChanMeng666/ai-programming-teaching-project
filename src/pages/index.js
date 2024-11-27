124e0c673c636487e7d0214c208e047af515938f
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-11-28 09:20:41 +1300

    Optimize home page style

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,52 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+// function HomepageHeader() {
+//   const {siteConfig} = useDocusaurusContext();
+//   return (
+//     <header className={clsx('hero', styles.heroBanner)}>
+//       <div className="container">
+//         <Heading as="h1" className="hero__title">
+//           掌握AI编程
+//         </Heading>
+//         <p className="hero__subtitle">
+//           通过实践案例学习人工智能开发
+//         </p>
+//         <div className={styles.buttons}>
+//           <Link
+//             className="button button--primary button--lg"
+//             to="/docs/intro">
+//             开始学习
+//           </Link>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// }
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <svg 
+        className="waves"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28"
+        preserveAspectRatio="none"
+        shapeRendering="auto"
+      >
+        <defs>
+          <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+        </defs>
+        <g className="parallax">
+          <use xlinkHref="#gentle-wave" x="48" y="0" />
+          <use xlinkHref="#gentle-wave" x="48" y="3" />
+          <use xlinkHref="#gentle-wave" x="48" y="5" />
+          <use xlinkHref="#gentle-wave" x="48" y="7" />
+        </g>
+      </svg>
+      
       <div className="container">
         <Heading as="h1" className="hero__title">
           掌握AI编程
@@ -19,7 +61,7 @@ function HomepageHeader() {
         </p>
         <div className={styles.buttons}>
           <Link
-            className="button button--primary button--lg"
+            className="button button--secondary button--lg"
             to="/docs/intro">
             开始学习
           </Link>
