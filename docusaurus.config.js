7327a302993d969d570853d027b28081e719e99f
ChanMeng666 <chanmeng.dev@gmail.com>
2025-07-15 22:43:04 +1200

    Fix index name to use actual existing pages index

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -112,8 +112,8 @@ const config = {
         // Public API key: it is safe to commit it - Search API Key
         apiKey: 'f70da822145b6159e198fc85147e0564',
 
-        // Crawler Name (使用主索引名称以搜索所有子索引)
-        indexName: 'ai_programming_teaching_project_vercel_app_8vhxtp609d',
+        // Crawler Name (使用实际存在的pages索引)
+        indexName: 'ai_programming_teaching_project_vercel_app_8vhxtp609d_pages',
 
         // Optional: see doc section below
         contextualSearch: true,
